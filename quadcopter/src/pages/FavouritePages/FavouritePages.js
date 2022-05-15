68b94258c3db8c5cecb79b30a41784cb4198c09c
Sergej Miroshnichenko <sergej.miroshnichenko.kr@gmail.com>
2022-05-16 02:08:17 +0300

    fix: add to cart in favourite

--- quadcopter/src/pages/FavouritePages/FavouritePages.js
+++ quadcopter/src/pages/FavouritePages/FavouritePages.js
@@ -1,11 +1,26 @@
 import Products from "../../components/Products/Products";
 
 
-const FavouritePages = ({data, toFavourite}) => {
+const FavouritePages = ({data, toFavourite, setCartItem}) => {
+
+    const addToCart = (name, price) => {
+        setCartItem((prev) => {
+            const index = prev.findIndex(item => item.name === name);
+            if (index === -1) {
+                return [...prev, {name, price, count: 1}]
+            } else {
+                const newState = [...prev]
+                newState[index].count += 1;
+                newState[index].isinCart = true;
+                // localStorage.setItem('cart', JSON.stringify(newState))
+                return newState;
+            }
+        })
+    }
 
     return (
         <div>
-            <Products data={data} toFavourite={toFavourite} />
+            <Products data={data} toFavourite={toFavourite} addToCart={addToCart}/>
         </div>
     )
 }
